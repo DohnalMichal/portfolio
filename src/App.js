ce9ecf37d7de48f8d46621f45b8834f6f6b4d13a
DohnalMichal <michal.dohnal7@gmail.com>
2021-03-05 10:43:24 +0100

    Add headline and spinner

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import React from "react";
+import { BarLoader } from "react-spinners";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        🚧 Porfolio under reconstruction! 🚧
+        <BarLoader className="spinner" loading width="200" height="10px" color="yellow" />
+        <div className="headline">
+          <h2>🚧 Porfolio under reconstruction! 🚧</h2>
+        </div>
       </header>
     </div>
   );
